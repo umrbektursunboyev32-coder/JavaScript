1be99d8866ec4ffb39f4723117493aad5db576a0
Sardor <sardorbekyangiboyev201029@gmail.com>
2025-12-06 17:01:33 +0500

    1v

---
+++ index.js
@@ -0,0 +1,514 @@
+// let x=10;
+// console.log(x, typeof(x));
+// let y='men';
+// console.log(y, typeof(y))
+// let z;
+// console.log(z, typeof(z))
+// let a=true;
+// a=false;
+// console.log(a, typeof(a))
+// let b=[1,2,3,4,5,6];
+// console.log(b, typeof(b))
+// let c={
+//     name: "Umrbek",
+//     age: 13
+// }
+// console.log(c, typeof(c))
+// document.getElementById('demo').innerHTML='Salom output';
+// document.getElementById('demo2').innerHTML='Salom h1';
+
+// let ism=prompt('ismingizni yozing')
+// let fam=prompt('Familya')
+// document.getElementById('demo').innerHTML="Sahifamizga hush kelibsiz"+' '+ ism+' '+fam;
+//     let javob=confirm('siz bizni shartlarimizga rozimisiz')
+//     if (javob){
+//         alert('Siz shartlarga rozi bo`ldingiz')
+//         document.getElementById('demo2').innerHTML="Siz shartlarga rozi bo`ldingiz"
+//         console.log('yangi foydalanuvchi:')
+//         console.log(fam)
+//         console.log(ism)
+//     }
+//     else{
+//         alert('siz shartlarga rozi bo`lmadingiz')
+//         confirm('Aniqmi fikringizni o`zgartirmaysizmi')
+//          document.getElementById('demo2').innerHTML="Siz shartlarga rozi bo`lmadingiz"
+//     }
+//     let w=20
+//     let q=15
+    // console.log(w+q);
+    // console.log(w-q);
+    // console.log(w*q);
+    // console.log(w/q);
+    // console.log(w%q);
+    // console.log(w**q);
+    // console.log(w>q);
+    // console.log(w<q);
+    // console.log(w>=q);
+    // console.log(w<=q);
+    // console.log(w!=q);
+    // console.log(w==q);
+    // let a=5
+    // let b=10
+    // let c=15
+    // let d=20
+    // // console.log(a>b && c>d);
+    // // console.log(a<b && c<d);
+    // // console.log(a>b && c<d);
+    // // console.log(a<b || c>d);
+    // // console.log(a>b || c>d);
+    // // console.log(!(a>b || c>d));
+    // let z=15
+    // console.log(z+=5)
+    // console.log(z=z+5)
+    // console.log(z-=5)
+    // console.log(z=z-5)
+    // console.log(z*=2)
+    // console.log(z=z*2)
+    // console.log(z/=5)
+    // console.log(z=z/2)
+    // console.log(z%=4)
+    // console.log(z**=5)
+    // let yosh= 14;
+    // let yosh2=18
+    // let natija=yosh2>=18 && !(yosh>=18) ?  console.log('Ha') : console.log("Yo`q")
+    // if(yosh>=18){
+    //     console.log("Balog`atga yetgan")
+    // }
+    // else{
+    //     console.log("Balog`atga yetmagan")
+    // }
+    // let ball=prompt('ball')
+    // if(ball>=86){
+    //     console.log("A'lo")
+    // }
+    // else if(ball>=70){
+    //     console.log("Yaxshi")
+    // }
+    // else if(ball>=50){
+    //     console.log("Qoniqarli")
+    // }
+    // else{
+    //     console.log("Qoniqarsiz")
+    // }
+    // let son1 = prompt('son')
+    // let son2 = prompt('son')
+    // console.log((son%2===0) ? 'Juft' : 'toq')    
+    // console.log(son>10 ? 'M': (son)? 'Manfiy': 'Nol')
+    // if(son1<son2){
+    //      console.log("kichik")
+    //     console.log(son1)
+    // }
+    // if(son2>son1){
+    //      console.log('Katta')
+    //     console.log(son2)
+    // }
+    // else{
+    //     console.log('teng')
+    // }
+    // let ln=prompt('Login kiriting')
+    // let pl=prompt('Parol kiriting')
+    // let fln=prompt('loginni tekshirish:')
+    // let fpl=prompt('Parolni tekshirish:')
+    // if(ln===fln && pl===fpl){
+    //     alert('Salom')
+    // }else{
+    //     alert('Xayr')
+    // }
+    // let soat=prompt('soat:');
+    // if(soat>=6 && soat<12){
+    //     console.log('Xayrli tong')
+    // }
+    // else if(soat>=12 && soat<18){
+    //     console.log('Xayrli kun')
+    // }
+    //    else if(soat>=18 && soat<22){
+    //     console.log('Xayrli kun')
+    // }
+    // else if(soat>0 && soat<6){
+    //     console.log('Xayrli tun')
+    // }
+    // let matn=prompt('So`z kirit')
+    // let teskari=matn.split('').reverse().join('')
+    // if(matn===teskari){
+    //     console.log('palindrom')
+    // }
+    // else{
+    //     console.log('palindrom emas')
+    // }
+    // let ism=prompt('Ism kiriting')
+    // let fam=prompt('Familya  kiriting')
+    // console.log(`familya: ${fam} \n ism: ${ism}`)
+    // let kun=prompt('kun')
+    // switch(kun){
+    //     case '1':
+    //         console.log('Dushanba')
+    //         break;
+    //      case '2':
+    //         console.log('Seshanba')
+    //         break;
+    //     case '3':
+    //         console.log('Chorshanba')
+    //         break;
+    //     case '4':
+    //         console.log('Payshanba')
+    //         break;
+    //     case '5':
+    //         console.log('Juma')
+    //         break;
+    //     case '6':
+    //         console.log('Shanba')
+    //         break;
+    //     case '7':
+    //         console.log('Yakshanba')
+    //         break;
+    //     default:
+    //         console.log('Bunday hafta kuni yo`q')
+    // }
+    // let son=prompt('son')
+    // console.log(typeof son)
+    //  let son1=Number(son)
+    // console.log(typeof son1)
+    // let son=prompt('son')
+    // console.log(typeof son)
+    // let sonint=parseInt(son)
+    // console.log(typeof sonint)
+    // let son2=prompt('son2')
+    // console.log(parseInt(son2 , 4))
+    // document.getElementById('demo2').innerHTML=parseInt(son2 , 4)
+    //     let kun=prompt('oy  kiriting')
+    // switch(kun){
+    //     case '1':
+    //         console.log('Qish')
+    //         break;
+    //      case '2':
+    //         console.log('Qish')
+    //         break;
+    //     case '3':
+    //         console.log('Bahor')
+    //         break;
+    //     case '4':
+    //         console.log('Bahor')
+    //         break;
+    //     case '5':
+    //         console.log('Bahor')
+    //         break;
+    //     case '6':
+    //         console.log('Yoz')
+    //         break;
+    //     case '7':
+    //         console.log('Yoz')
+    //         break;
+    //     case '8':
+    //         console.log('Yoz')
+    //         break;
+    //     case '9':
+    //         console.log('Kuz')
+    //         break;
+    //     case '10':
+    //         console.log('Kuz')
+    //         break;
+    //     case '11':
+    //         console.log('Kuz')
+    //         break;
+    //     case '12':
+    //         console.log('Qish')
+    //         break;
+    //     default:
+    //         console.log('bunday oy yo`q')
+    // }
+    // let til=prompt('tilni tanlang: UZ, RU, Ing, Isp, Turk, Xitoy:')
+    // switch(til){
+    //     case'UZ':
+    //     document.getElementById('demo2').innerHTML="Salom"
+    //     break;
+    //      case'Ing':
+    //     document.getElementById('demo2').innerHTML="Hello"
+    //     break;
+    //      case'RU':
+    //     document.getElementById('demo2').innerHTML="привэт"
+    //     break;
+    //      case'Isp':
+    //     document.getElementById('demo2').innerHTML="Hola"
+    //     break;
+    //     case'Turk':
+    //     document.getElementById('demo2').innerHTML="Merhaba"
+    //     break;
+    //     case'Xitoy':
+    //     document.getElementById('demo2').innerHTML="“你好” (nǐ hǎo)"
+    //     break;
+    //     default:
+    //         alert('Bunday til sahifada yo`q')
+    // }
+    // let son1=Number(prompt('son1 ni kiriting'))
+    // let son2=Number(prompt('son2 ni kiriting'))
+    // let amal=prompt('Amalni kiriting')
+    // switch(amal){
+    //     case'+':
+    //     console.log(son1+son2)
+    //     break;
+    //     case'*':
+    //     console.log(so*+son2)
+    //     break;
+    //     case'/':
+    //     console.log(son1/son2)
+    //     break;
+    //     case'-':
+    //     console.log(son1-son2)
+    //     break;
+    // }
+    //     const display = document.getElementById("display");
+
+    // function appendValue(value) {
+    //   display.value += value;
+    // }
+
+    // function clearDisplay() {
+    //   display.value = "";
+    // }
+
+    // function calculate() {
+    //   try {
+    //     display.value = eval(display.value);
+    //   } catch {
+    //     display.value = "Xatolik!";
+    // //   }
+    // }
+    // let n=Number(prompt('Raqam'))
+    // for(let i=1; i<=10; i++){
+    //   for(let j=1; j<=10; j++){
+    //   console.log(`${i} X ${j}= ${i*j}`)
+    //   }
+    // }
+    // let n=prompt('son:')
+    // console.log(parseInt(n, 2))
+    // for(let i=1; i<=2; i++){
+    //   for(let j=3; j<=4; j++){
+    //     console.log(`${i}`)
+    //   }
+    // }
+    // let n=100
+    // let a=0,b=1
+    // for(let i=0; i<n;i++){
+    //     console.log(a)
+    //     let c=a+b
+    //     a=b
+    //     b=c
+    // }
+    // let n=Number(prompt('son'))
+    // let a=0;sanoqchi=0
+    // for(let i=1;i<=n;i++){
+    //     if(i%3==0){
+    //         a+=i;
+    //         sanoqchi++;
+    //         console.log(i)
+    //         console.log(sanoqchi)
+    //     }
+    // }
+    // console.log(a/sanoqchi)
+    // let n=Number(prompt('son'))
+    // let a=0;sanoqchi=0
+    // for(let i=1;i<=n;i++){
+    //     if(i%3==0 && i%5==0){
+    //         a+=i;
+    //         sanoqchi++;
+    //         console.log(i)
+    //         console.log(sanoqchi)
+    //     }
+    // }
+    // console.log(a/sanoqchi)
+    // let a=prompt('son')
+    // for(let i=2;i=a;i++){
+    //     for(let j=1;j=i;j++){
+    //        console.log(i)
+    //     }
+    // }
+    //11.11.2025
+    // const cars1=["Ravon","Cherry","Chevrolet"];
+    // const cars2=["Mersidenz-benz","BMW","Bugatti"];
+    // console.log(cars1);
+    // console.log(cars2);
+    // console.log(cars1)
+    // console.log(cars1.at(-0))
+    // let meva=['olma','gilos','olcha','nok','uzum','behi']
+    // console.log(meva)
+    // console.log(meva.push('anor'))
+    // console.log(meva.unshift('o`rik'))
+    //    let sonlar=[[1,2], [3,4] , [5,6]]
+    //    console.log(sonlar)
+    //    console.log(sonlar.flat())
+
+    //    let sons=[1,2,[3,4,[5,6,[7,8,[9,10]]]]]
+    //    console.log(sons)
+    //    console.log(sons.flat(4))
+    // let meva2=['olma','gilos','olcha','nok','uzum','behi']
+    // console.log(meva2)
+    // // console.log(meva2.splice(2,3))
+    // let meva3=meva2.toSpliced(3,2)
+    // console.log(meva3)
+    // let meva4=meva2.slice(2,5)
+    // console.log(meva4)
+    // const cars1=["Ravon","Cherry","Chevrolet","Mersidenz-benz"];
+    // const cars2=["Mersidenz-benz","BMW","Bugatti"];
+    // let sinf=cars2.concat(cars1)
+    // console.log(sinf)
+    // console.log(sinf.indexOf('Mersidenz-benz',2))
+    // console.log(sinf.lastIndexOf('Mersidenz-benz',2))
+    // console.log(sinf.includes('Bugatti'))
+    // let son1=[10,20,30,40,50,60,]
+    // console.log(son1.find((a) => (a+30)%9==1))
+    // console.log(son1.findIndex((b) => (b-30)/3==10))
+    // let son1=[10,20,30,40,50,60,]
+    // // console.log(son1.filter((i) => i%3==0))
+    // console.log(son1.some((i) => i>=30))
+    // console.log(son1.every((i) => i>20))
+    // let person=[
+    //     {ism: 'Maftuna', fam: 'Sharipova', baho: 47},
+    //     {ism: 'Umrbek', fam: 'Tursunboyev', baho: 50},
+    //     {ism: 'Elbek', fam: 'Raximberganov', baho: 50},
+    //     {ism: 'Islombek', fam: 'Sobirov', baho: 46},
+    // ]
+    // console.log(person)
+    // console.log(person.filter((i) => i.baho > 47))
+    // let guruh2=['Mirzobek','Maftuna','Umrbek','Elbek','Islombek']
+    // console.log(guruh2.sort())
+    // let son2=[2,5,10,7,3,80,6,9,40]
+    // console.log(son2)
+    // console.log(son2.sort())
+    // console.log(son2.sort((a,b) => b-a))
+    // let meva2=['olma','gilos','olcha','апелсин',1,'Nok','uzum','behi']
+    // let alfa=['A','b','C','d','G',1,5,'л','о']
+    // console.log(meva2.sort())
+    // console.log(meva2.sort((a,b) => a.localeCompare(b)))
+    // let son=[1,2,3,4,5,6,7,8,9]
+    // console.log(son.sort(() => 0.5- Math.random()))
+    // let sonlar=[12,5,8,130,44,27,33,90]
+    // console.log(sonlar.filter((i) => i%2==0).sort((a,b) => a-b))
+    //   let matn='JavaScript dasturlash tili juda qiziqarli va kuchli'
+    //   console.log(matn.split(' ').filter(a => a.length >= 5).sort((a,b) => a.length-b.length))
+    //let oquvchi=[
+    //    {ism:'Ali',baho:95},
+    //    {ism:'Vali',baho:78},
+    //    {ism:'Salim',baho:88},
+    //    {ism:'Zokir',baho:65},
+    //]
+    //console.log(oquvchi)
+    //oquvchi.forEach((qiymat, index) =>{
+    //    console.log(`${index+1}) ${qiymat.ism} - ${qiymat.baho} ball`)
+    //})
+    // let mson=[1,2,3,4,5]
+    // console.log(mson.map(a=> a**3))
+    // mson.forEach((a) =>{
+    //     console.log(a ** 3)
+    // })
+
+//let matn = "JavaScript dasturlash tili juda qiziqarli va kuchli"
+
+//console.log(matn)
+//console.log(matn.split(' ').filter((a) => a.length))
+
+
+// let mevalar=['olma','anor','gilos','shaftoli'];
+
+// mevalar.forEach((a, index) => {
+//     console.log(` ${index+1}- meva nomi: ${a}`)
+// })
+
+// let mson=[1,2,3,4,5];
+// mson.forEach((a,index) => {
+//     console.log(` ${a**3} `)
+// })
+
+
+// let oquvchi=[
+//     {ism:'Ali',baho:95},
+//     {ism:'Vali',baho:78},
+//     {ism:'Salim',baho:88},
+//     {ism:'Zokir',baho:65},
+// ]
+// console.log(oquvchi)
+// oquvchi.forEach((qiymat, index) =>{
+//     console.log(`${index+1}) ${qiymat.ism} - ${qiymat.baho} ball`)
+// })
+
+// let ishchilar=[
+//     {ism:'Ali',maosh:500},
+//     {ism:'Vali',maosh:700},
+//     {ism:'Salim',maosh:900},
+//     {ism:'Zokir',maosh:1200}
+// ]
+// console.log(ishchilar)
+// ishchilar.forEach((a,b) =>{
+//     console.log(`$(b+1) ${a.ism} - ${a.maosh + a.maosh/10} som`)
+// })
+
+// let ishchilar=[
+//     {nomi:'Ali',maosh:500},
+//     {nomi:'Vali',maosh:700},
+//     {nomi:'Salim',maosh:900},
+//     {nomi:'Zokir',maosh:1200},
+// ];
+// console.log(ishchilar)
+// ishchilar.forEach((a,b) =>{
+//     console.log(` ${b+1}) ${a.nomi} - ${a.maosh+a.maosh/10}som`)
+// })
+// let mahsulotlar=[
+//     {nomi:'Noutbuk ',narx:2000},
+//     {nomi:'Telefon ',narx:1500},
+//     {nomi:'Planshet',narx:1000},
+//     {nomi:'Quloqchin ',narx:200},
+// ];
+// console.log(mahsulotlar)
+// mahsulotlar.forEach((a,b) =>{
+//     console.log(` ${b+1}) ${a.nomi} asl narxi${a.narx}  chegirma bilan:${a.narx - a.narx/5}som`)
+//     })
+// let odamlar=[
+//     {ism:'Odilbek',fam:'Raximov'},
+//     {ism:'Baxtiyor',fam:'Saliyev'},
+//     {ism:'Xayotbek',fam:'Sultonmurodov'},
+//     {ism:'Umrbek',fam:'Tursunboyev'}
+// ]
+// console.log(odamlar.map(a => `${a.ism} ${a.fam}`))
+
+// let odam={
+//     ism: 'Quvonchbek',
+//     fam: 'Omonboyev',
+//     age: '15',
+//     map: 'Bogolon',
+//     maktab:14,
+//     info: function(){
+//         return `${this.ism} ${this.fam} - ${this.maktab}- maktabda oqiydi`
+//     }
+
+// }
+// console.log(odam.info())
+// odam.ism='Quvonchbek'
+// odam.fam='Amanboyev'
+// odam['maktab']=33;
+// odam['map']='senter'
+// delete odam.map
+// console.log(odam)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
